ed8ea5e1384b656f58fee659dd85b8701200e0d2
Rachel Bolton <50831008+rachel-e-bolton@users.noreply.github.com>
2020-10-26 15:00:22 +0200

    All DB methods working and tested.

--- src/main/java/swingy/Swingy.java
+++ src/main/java/swingy/Swingy.java
@@ -2,11 +2,6 @@ package swingy;
 
 import controllers.database.Database;
 import helpers.TextColors;
-import models.gamestate.GameState;
-
-import javax.xml.crypto.Data;
-import java.io.PrintWriter;
-import java.util.ArrayList;
 
 public class Swingy {
 
